5f46879c829aaab518de9b6038132d9abdbb4925
sabrinara <you@example.com>
2023-11-08 19:20:41 +0600

    Exclusive services section  is created and modifided

---
+++ src/components/other/ExServices.jsx
@@ -0,0 +1,49 @@
+
+
+const ExServices = () => {
+    return (
+        <div>
+            <hr className=" border-sky-400" />
+            <h1 className="text-7xl font-bold text-center my-16 ">Exclusive Services</h1>
+            <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-10 justify-center ">
+
+                <div className="relative grid h-[40rem] w-full max-w-[28rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
+                    <div className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://i.ibb.co/3hHxK7L/a-2.jpg')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
+                        <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
+                    </div>
+                    <div className="relative p-6 px-6 py-14 md:px-12">
+                        <h2 className="mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased">
+                            Do you want our professional cleaner to clean your home? 
+                        </h2>
+                        
+                    </div>
+                </div>
+                <div className="relative grid h-[40rem] w-full max-w-[28rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
+                    <div className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://i.ibb.co/PwbrjP8/paste-control.jpg')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
+                        <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
+                    </div>
+                    <div className="relative p-6 px-6 py-14 md:px-12">
+                        <h2 className="mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased">
+                            We will protect your home from any kind of insecta. Grab our exclusive services?
+                        </h2>
+                        
+                    </div>
+                </div>
+                <div className="relative grid h-[40rem] w-full max-w-[28rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
+                    <div className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://i.ibb.co/9b3s860/gardening.jpg')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
+                        <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
+                    </div>
+                    <div className="relative p-6 px-6 py-14 md:px-12">
+                        <h2 className="mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased">
+                            Love gardening? We can help you to meet your desires.
+                        </h2>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default ExServices;
