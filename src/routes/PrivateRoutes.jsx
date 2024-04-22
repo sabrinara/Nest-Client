b2be3793087992af2899ba0170825b9fc4121a45
sabrinara <saashid18@gmail.com>
2024-04-22 21:47:31 +0600

    loader issues fixed

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
-
+import { RiseLoader } from "react-spinners";
 
 const PrivateRouters = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
@@ -9,7 +9,14 @@ const PrivateRouters = ({ children }) => {
     console.log(location.pathname);
 
     if (loading) {
-        return <span className="loading loading-bars loading-lg"></span>
+        return (
+            <RiseLoader
+            className="text-center items-center py-40"
+           color="rgb(2 132 199)"
+            size={20}
+            speedMultiplier={1}
+          />
+        )
     }
 
     if (user) {
